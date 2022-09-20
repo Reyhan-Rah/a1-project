421a557407f163cdab8302be33b0fb0cece617f6
Reyhaneh Rahmanipour <r.rahmanipour@gmail.com>
2022-09-20 12:16:59 +0430

    set footer link auto1

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,7 +7,7 @@ export const Footer = () => {
       <div className="flex w-full justify-center p-4 bg-white items-center md:p-6 dark:bg-gray-800 h-20">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           ©{" "}
-          <a href="src/components/footer" className="hover:underline">
+          <a href="https://www.auto1.com" className="hover:underline">
             AUTO1 Group
           </a>{" "}
           2018
