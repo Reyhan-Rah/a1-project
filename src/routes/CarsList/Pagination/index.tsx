7ae067948fe9d79ebde64d9ac753a755a712da0c
Reyhaneh Rahmanipour <r.rahmanipour@gmail.com>
2022-09-21 08:43:17 +0430

    fix pagination responsive styles

--- src/routes/CarsList/Pagination/index.tsx
+++ src/routes/CarsList/Pagination/index.tsx
@@ -18,38 +18,43 @@ const usePaginationClick = () => {
 export const Pagination = ({ totalPageCount }: IPagination) => {
   const { currentPage, onClick } = usePaginationClick();
   return (
-    <div className={"inline-flex w-full items-center justify-center "}>
+    <div
+      className={
+        "inline-flex w-full items-center justify-center p-1 lg:p-3 gap-3 lg:gap-6"
+      }
+    >
       <button
         onClick={() => onClick(1)}
-        className="inline-flex items-center py-2 px-4 text-sm font-medium text-light-orange hover:text-dark-orange hover:underline dark:text-light-gray dark:hover:text-white disabled:text-dark-gray"
+        disabled={currentPage === 1}
+        className="inline-flex items-center text-sm font-medium text-light-orange hover:text-dark-orange hover:underline dark:text-light-gray dark:hover:text-white disabled:text-dark-gray dark:disabled:text-gray-400 disabled:no-underline"
       >
         First
       </button>
       <button
         onClick={() => onClick(Math.max(currentPage - 1, 0))}
         disabled={currentPage === 1}
-        className="inline-flex items-center py-2 px-4 text-sm font-medium text-light-orange hover:text-dark-orange hover:underline dark:text-light-gray dark:hover:text-white disabled:text-dark-gray dark:disabled:text-gray-400"
+        className="inline-flex items-center text-sm font-medium text-light-orange hover:text-dark-orange hover:underline dark:text-light-gray dark:hover:text-white disabled:text-dark-gray dark:disabled:text-gray-400 disabled:no-underline"
       >
         Previous
       </button>
       <p
         className={
-          "inline-flex items-center py-2 px-4 text-sm font-medium text-dark-gray dark:text-light-gray"
+          "inline-flex items-center text-sm font-medium text-dark-gray dark:text-light-gray"
         }
       >
         Page {currentPage} of {totalPageCount}
       </p>
       <button
         onClick={() => onClick(Math.min(currentPage + 1, totalPageCount))}
-        className="inline-flex items-center py-2 px-4 text-sm font-medium text-light-orange hover:text-dark-orange hover:underline dark:text-light-gray dark:hover:text-white disabled:text-dark-gray dark:disabled:text-gray-400"
         disabled={currentPage === totalPageCount}
+        className="inline-flex items-center text-sm font-medium text-light-orange hover:text-dark-orange hover:underline dark:text-light-gray dark:hover:text-white disabled:text-dark-gray dark:disabled:text-gray-400 disabled:no-underline"
       >
         Next
       </button>
       <button
         onClick={() => onClick(totalPageCount)}
-        className="inline-flex items-center py-2 px-4 text-sm font-medium text-light-orange hover:text-dark-orange hover:underline dark:text-light-gray dark:hover:text-white disabled:text-dark-gray"
         disabled={currentPage === totalPageCount}
+        className="inline-flex items-center text-sm font-medium text-light-orange hover:text-dark-orange hover:underline dark:text-light-gray dark:hover:text-white disabled:text-dark-gray dark:disabled:text-gray-400 disabled:no-underline"
       >
         Last
       </button>
