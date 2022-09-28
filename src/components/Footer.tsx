1042f4d7f83d8da505ff1018ba5ac6e1eae81790
Reyhaneh Rahmanipour <r.rahmanipour@gmail.com>
2022-09-28 18:40:42 +0330

    fix href link in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
       <div className="flex w-full justify-center p-4 bg-white items-center md:p-6 dark:bg-gray-800 h-20">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           ©{" "}
-          <a href="#" className="hover:underline">
+          <a href="/" className="hover:underline">
             AUTO1 Group
           </a>{" "}
           2018
